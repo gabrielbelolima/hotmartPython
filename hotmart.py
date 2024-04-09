bbbf8b44b6396f34d4502e0ae382e6a7e72168c1
gabriel.lima <gabriel.lima@fluencyacademy.io>
2024-04-09 18:00:41 -0300

    Update class
    Updates hotmartClass

--- hotmart.py
+++ hotmart.py
@@ -8,7 +8,7 @@ import requests
 import pandas as pd
 from tqdm.notebook import tqdm
 from time import sleep
-from datetime import datetime as dt, timedelta
+from datetime import timedelta
 
 class HotMart:
 
@@ -75,7 +75,7 @@ class HotMart:
         
         if transaction != '':
             params_s = {
-                'transaction': transaction
+                'transaction': transaction,
             }
         else:        
             params_s = {
@@ -91,35 +91,31 @@ class HotMart:
         
         while True:
             
-            sales_data, page_info, resp_headers = self.chamar_api(params_s = params_s, endpoint = endpoint)           
+            sales_data, page_info, resp_headers = self.chamar_api(params_s = params_s, endpoint = endpoint)
+
             all_sales_data.extend(sales_data) # Adiciona dados de vendas
             next_page_token = page_info.get('next_page_token') # Atualiza next_page_token nos parâmetros
             
             if int(resp_headers['RateLimit-Remaining']) <= 25:
                 #print(resp_headers['RateLimit-Remaining']) #DEBUG
                 sleep(int(resp_headers['RateLimit-Reset'])+5)
-            if pbar is True:
+            if pbar == True:
                 # progress bar
                 if len(all_sales_data) >0: # Evita divisão por zero!
                     if switch == 0: # Garante que o total_runs será definido apenas uma vez!
-                        total = int(page_info['total_results']/100)+1
-                        pbar_tqdm = tqdm(total = total, colour = '#2CD0E5', leave = False, desc = 'DEBUG..')
+                        pbar = tqdm(total = int(page_info['total_results']/100)+1, colour = '#2CD0E5', leave = False)
                 # incrementa pbar e ativa switch (após o primeiro loop)
                 switch = 1
-                pbar_tqdm.update(1)
-                
+                pbar.update(1)
+                 
                 # Atualiza token da página para próxima chamada, se houver
                 if next_page_token:
                     params_s.update({'page_token':next_page_token})
                 else:
                     break
             else:
-                #pbar is False
-                if next_page_token:
-                    params_s.update({'page_token':next_page_token})
-                else:
-                    break
-            
+                break
+          
         # Criar DataFrame a partir dos dados coletados
         dataframe = pd.DataFrame(all_sales_data)
         
@@ -131,7 +127,7 @@ class HotMart:
         
         dfs_t = []
         not_found = []
-        for t in tqdm(transactions, leave = False, desc = 'Transactions..'):
+        for t in tqdm(transactions, leave = True, desc = 'Transactions..'):
             try:
                 dfs_t.append(self.get_sales_hm(transaction = t))
             except:
@@ -148,7 +144,7 @@ class HotMart:
         
         if len(dfs_t) > 1:
             df = pd.concat(dfs_t, ignore_index = True)
-            df.loc[:,[c for c in df.columns if 'date' in c]] = df.loc[:,[c for c in df.columns if 'date' in c]].apply(lambda x: pd.to_datetime(x, unit = 'ms'))
+            df.loc[:,[c for c in df.columns if 'date' in c]] = df.loc[:,[c for c in df.columns if 'date' in c]].apply(lambda x: pd.to_datetime(x))
         
         if return_not_found == True:
             return df, not_found
@@ -182,14 +178,14 @@ class HotMart:
     
 
     def get_df_from_json_serie(self, serie = pd.Series):
-        df_aux = pd.concat([pd.DataFrame([x]) for x in tqdm(serie, leave = False, desc = 'Loading json..')])
+        df_aux = pd.concat([pd.DataFrame([x]) for x in tqdm(serie, leave = False)])
         df = df_aux.reset_index(drop = True)
         
         return df
     
     
 # - Main Function (Executa loop na paginação)
-    def get_sales_hm(self, start_date=None, end_date= None, formato = '%Y-%m-%d', endpoint = None, transaction = '', pbar = True):
+    def get_sales_hm(self, start_date=None, end_date= None, transaction = '', formato = '%Y-%m-%d', endpoint = None, pbar = True):
             
         # Chama API do hotmart
         if transaction != '':
@@ -204,13 +200,11 @@ class HotMart:
         try:
             lst_col_dict = {k:v for k,v in dct_types.items() if 'dict' in str(v)}.keys()
             # Abre colunas json dos dfs carregados:
-            for c in tqdm(lst_col_dict, leave = False, desc = 'Loading Sales..'):
+            for c in tqdm(lst_col_dict, leave = False):
                 df = self.join_json_col(df, c)
                 
         except: # Se não houver nenhuma, siga a diante.
             print('Colunas json não encontradas\n', f'len(lst_col_dict) = {len(lst_col_dict)}')
             pass
-        
-        # Transforma colunas de data
-        df.loc[:, [x for x in df.columns if 'date' in x.lower()]] = df.loc[:, [x for x in df.columns if 'date' in x.lower()]].apply(lambda x: x.apply(lambda y: dt.fromtimestamp(y/1000)))
+
         return df
